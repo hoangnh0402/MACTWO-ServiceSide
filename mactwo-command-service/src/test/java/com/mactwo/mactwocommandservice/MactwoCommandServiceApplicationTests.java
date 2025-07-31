218c8127f2bd3917cd9595f7a760fff6b53c534a
hoangnh4204 <hoangnh4204@gmail.com>
2025-07-31 11:12:40 +0700

    chore(command): init domain and infrastructure layer structure

---
+++ mactwo-command-service/src/test/java/com/mactwo/mactwocommandservice/MactwoCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mactwo.mactwocommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MactwoCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
