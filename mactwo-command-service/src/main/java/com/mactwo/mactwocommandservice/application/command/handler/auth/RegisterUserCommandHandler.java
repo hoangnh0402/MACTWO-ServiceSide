a0ee68d91ab077828116108fe45414227f191b43
hoangnh4204 <hoangnh4204@gmail.com>
2025-08-01 14:40:36 +0700

    fix: debug don't add roles when create user

--- mactwo-command-service/src/main/java/com/mactwo/mactwocommandservice/application/command/handler/auth/RegisterUserCommandHandler.java
+++ mactwo-command-service/src/main/java/com/mactwo/mactwocommandservice/application/command/handler/auth/RegisterUserCommandHandler.java
@@ -3,7 +3,10 @@ package com.mactwo.mactwocommandservice.application.command.handler.auth;
 import com.mactwo.contants.TopicConstant;
 import com.mactwo.event.UserRegisteredEvent;
 import com.mactwo.mactwocommandservice.application.command.command.user.RegisterUserCommand;
+import com.mactwo.mactwocommandservice.domain.model.ERole;
+import com.mactwo.mactwocommandservice.domain.model.Role;
 import com.mactwo.mactwocommandservice.domain.model.User;
+import com.mactwo.mactwocommandservice.domain.repository.RoleRepository;
 import com.mactwo.mactwocommandservice.domain.repository.UserRepository;
 import com.mactwo.mactwocommandservice.infrastructure.mapper.UserMapper;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -17,13 +20,15 @@ public class RegisterUserCommandHandler {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public RegisterUserCommandHandler(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, KafkaTemplate<String, Object> kafkaTemplate) {
+    public RegisterUserCommandHandler(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder, KafkaTemplate<String, Object> kafkaTemplate, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
         this.kafkaTemplate = kafkaTemplate;
+        this.roleRepository = roleRepository;
     }
 
     @Transactional
@@ -39,6 +44,11 @@ public class RegisterUserCommandHandler {
         user.setDateOfBirth(command.getDateOfBirth());
         user.setGender(command.getGender());
         user.setPasswordHash(passwordEncoder.encode(command.getPassword()));
+
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Default role not found"));
+
+        user.getRoles().add(userRole);
 
         User savedUser = userRepository.save(user);
 
